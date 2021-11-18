b659188fce8310f18734a5e7da13bccf5abd6d65
as.zaycev <as.zaycev@tensor.ru>
2021-11-18 10:35:45 +0300

    добавил описание заданий в ts

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -8,12 +8,20 @@ interface IStudentData extends IPersonData {
     phone: string;
 }
 
+/*
+1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IPersonData
+2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IPersonData
+ */
 export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
 }
 
+/*
+1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IStudentData
+2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IStudentData
+ */
 export class Student extends Person {
     readonly _phone: string;
 }
